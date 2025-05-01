e4e97654717a9aa2ea13af278fdb9d2e4269b41b
Noemi Baglieri <noemibaglieri@gmail.com>
2025-05-01 18:35:04 +0200

    finished tasks

--- details.js
+++ details.js
@@ -15,8 +15,10 @@ fetch(photoDetail + photoId, {
     const container = document.getElementById("pexels-content");
     const h1 = document.createElement("h1");
     h1.innerText = picture.photographer;
+    h1.className = "mt-4 mb-4 bg-white rounded-5 ps-4 pe-4 pt-2 pb-2";
 
     const img = document.createElement("img");
+    img.className = "rounded-3";
     img.src = picture.src.original;
     img.style.width = "600px";
     img.style.maxWidth = "100%";
